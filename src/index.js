6d2bcefd951028199d877a51c2579ae482870f8f
Nick Hape <hape.nick@gmail.com>
2021-02-04 14:18:46 +0100

    added a constructor with props and super(props)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  // FIRST method called any time an instance of a class is created in JS
+  constructor(props) {
+    // reference to the parent's constructor function
+    super(props);
+
+    this.state = {lat: null, };
+  }
+  // React makes us always define "render"
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
