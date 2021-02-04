f5b084e66347e448b7e7c461c49450b52116885c
Nick Hape <hape.nick@gmail.com>
2021-02-04 15:07:56 +0100

    conditional display of different paths PLUS a loading screen

--- src/index.js
+++ src/index.js
@@ -21,14 +21,16 @@ class App extends React.Component {
   }
   // React makes us always define "render"
   render() {
-    return (
-      <div>
-        Latitude: {this.state.lat}
-        <br />
-        Error: {this.state.errorMessage}
-      </div>
-      );
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading...⏳ </div>
   }
-};
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
