3e1ca2f359d72d1ef3b984243c12240df72c98bc
Nick Hape <hape.nick@gmail.com>
2021-02-05 17:13:18 +0100

    added relevant icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -2,18 +2,23 @@ import React from 'react';
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
-    return lat > 0 ? '☀️' : '☃️';
+    return lat > 0 ? 'summer' : 'winter';
   } else {
-    return lat > 0 ? '☀️' : '☃️';
+    return lat > 0 ? 'summer' : 'winter';
   }
 };
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  console.log(season);
+  const text = season === 'winter' ? 'Ice, ice, baby ❄️' : 'I need me some vitamin sea ☀️🌊';
+  const icon = season === 'winter' ? 'sun outline' : 'snowflake outline';
 
   return (
-      <div>SeasonDisplay is linked!</div>
+      <div>
+        <i className={`spinner ${icon} icon`}/>
+        <h1>{text}</h1>
+        <i className={`spinner ${icon} icon`}/>
+      </div>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -3,21 +3,14 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  // FIRST method called any time an instance of a class is created in JS
-  constructor(props) {
-    // reference to the parent's constructor function
-    super(props);
-    // this is the ONLY time we do a direct assignment to this.state!
-    this.state = {lat: null, errorMessage: "" };
-
-  };
+  state = { lat: null, errorMessage: '' };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ lat: position.coords.latitude }),
       err => this.setState({ errorMessage: err.message })
-      );
-  };
+    );
+  }
 
   // React makes us always define "render"
   render() {
@@ -26,11 +19,11 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading...⏳ </div>
+    return <div>Loading <i className="spinner loading icon"></i> </div>;
   }
-};
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
